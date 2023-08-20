ed16b7c06b0cfc80a0f70af7da625513f1f50af6
heyset <c.inaciomatheus@gmail.com>
2023-08-20 03:41:06 -0300

    test

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ import cors from 'cors';
 import path from 'path';
 
 export const app = express();
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      'default-src': 'self',
+      'img-src': '*',
+    },
+  },
+}));
 app.use(cors());
 app.use(express.static(path.join(process.cwd(), 'src', 'public')));
