319bd273b61bc60b9c2d3922b6e3dff1ec3e3d45
heyset <c.inaciomatheus@gmail.com>
2023-08-20 03:43:32 -0300

    removes helmet

--- src/server.js
+++ src/server.js
@@ -4,13 +4,5 @@ import cors from 'cors';
 import path from 'path';
 
 export const app = express();
-app.use(helmet({
-  contentSecurityPolicy: {
-    directives: {
-      'default-src': 'self',
-      'img-src': '*',
-    },
-  },
-}));
 app.use(cors());
 app.use(express.static(path.join(process.cwd(), 'src', 'public')));
